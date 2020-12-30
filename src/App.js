08b58675f6f6d8d452b5c0d588d9b0f483b33667
Petar <petarstojkovic92@gmail.com>
2020-12-30 14:01:56 +0100

    6. Refactoring Code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { loadGames } from "./actions/gamesAction";
+// Importing components and pages
+import Home from "./pages/Home";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadGames());
-  });
-
   return (
     <div className='App'>
       <h1>Hello Ignite</h1>
+      <Home />
     </div>
   );
 }
