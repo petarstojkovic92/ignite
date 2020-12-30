7107ae05d6859bea0de02d9d2e80f07e62039a9f
Petar <petarstojkovic92@gmail.com>
2020-12-30 16:36:47 +0100

    8. Global Styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // Importing components and pages
 import Home from "./pages/Home";
+// Importing styles
+import GlobalStyles from "./components/GlobalStyles";
 
 function App() {
   return (
     <div className='App'>
+      <GlobalStyles />
       <Home />
     </div>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -34,6 +34,30 @@ const Home = () => {
           />
         ))}
       </Games>
+      <h2>Popular Games</h2>
+      <Games>
+        {popular.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            image={game.background_image}
+            id={game.id}
+            key={game.id}
+          />
+        ))}
+      </Games>
+      <h2>New Games</h2>
+      <Games>
+        {newGames.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            image={game.background_image}
+            id={game.id}
+            key={game.id}
+          />
+        ))}
+      </Games>
     </GameList>
   );
 };
